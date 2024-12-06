835e684ad39af3e1a9b024a85650ddbe4f337534
Zarya Ajasin <irawo10@gmail.com>
2024-12-06 12:30:06 -0500

    edits

--- sketch.js
+++ sketch.js
@@ -27,12 +27,16 @@ function draw() {
   let herbal = color(245, 240, 203);
   let start = color(255, 255, 255);
   textSize(24);
+  textAlign(CENTER);
   fill(10, 10, 10);
+
+  steeptime = 0;
+  dayColorOffset = 0.002; //basically 1 min ig, a bit less than a min
   
   //brewing process
   if(keyIsDown(32)){
     if(i <= 1) {
-      i= i + dayColorOffset; //0.05 was 3 sec, 0.005
+      i= i + dayColorOffset; //0.05 was 5 sec, 0.005 was 20
       background(interA);
     } else {
       print('color progress finished')
@@ -45,8 +49,6 @@ function draw() {
   // time of day
   if (hour() < 12 && hour() > 2) {
     interA = lerpColor(start, black, i);
-    steeptime = 4;
-    dayColorOffset = 0.0004;
 
     if (startSteep == false) {
       fill(10, 10, 10);
@@ -66,13 +68,11 @@ function draw() {
 
   } else if (hour() < 17 && hour() > 12) {
     interA = lerpColor(start, green, i);
-    steeptime = 3;
-    dayColorOffset = 0.0009;
     if (startSteep == false) {
       fill(10, 10, 10);
       textSize(24);
-      text('good afternoon. hold space bar to brew your tea.', (windowWidth/2)-200, (windowHeight/2)+250);
-      text('today we have green tea for focus.', (windowWidth/2)-175, (windowHeight/2)+300);
+      text('good afternoon. hold space bar to brew your tea.', (windowWidth/2), (windowHeight/2)+250);
+      text('today we have green tea for focus.', (windowWidth/2), (windowHeight/2)+300);
     }
 
     if(brewing == true) {
@@ -80,20 +80,18 @@ function draw() {
       if (keyIsDown(32)) {
         
       } else if (brewed == false) {
-        text('hold space to keep brewing', (windowWidth/2)-125, (windowHeight/2)+250);
+        text('hold space to keep brewing', (windowWidth/2), (windowHeight/2)+250);
       }
     }
     
   } else {
     interA = lerpColor(start, herbal, i);
-    steeptime = 2;
-    dayColorOffset = 0.002;
     print('night');    
     if (startSteep == false) {
       fill(10, 10, 10);
       textSize(24);
-      text('good evening. hold space bar to brew your tea.', (windowWidth/2)-200, (windowHeight/2)+250);
-      text('tonight we have herbal tea for relaxation.', (windowWidth/2)-175, (windowHeight/2)+300);
+      text('good evening. hold space bar to brew your tea.', (windowWidth/2), (windowHeight/2)+250);
+      text('tonight we have herbal tea for relaxation.', (windowWidth/2), (windowHeight/2)+300);
     }
     
     if(brewing == true) {
@@ -101,39 +99,36 @@ function draw() {
       if (keyIsDown(32)) {
         
       } else if (brewed == false) {
-        text('hold space to keep brewing', (windowWidth/2)-125, (windowHeight/2)+250);
+        text('hold space to keep brewing', (windowWidth/2), (windowHeight/2)+250);
       }
     }
   }
 
   // checking if brewed
   print('time seconds is ' + timeSecond);
-  if (timeSecond < 10 && steeptime > 1) {
+  print('steeptime is ' + steeptime);
+  
     if (steeptime == 0 && timeSecond == -1){
       print("im brewed!")
-      text('your tea is ready.', (windowWidth/2)-90, (windowHeight/2)+250);
-      text('come back soon for another cup', (windowWidth/2)-90, (windowHeight/2)+300);
+      text('your tea is ready.', (windowWidth/2), (windowHeight/2)+250);
+      text('come back soon for another cup', (windowWidth/2), (windowHeight/2)+300);
       brewed = true;
     } else {
-      print(steeptime + ":0" + timeSecond);
-      text('your tea will be ready in ' +steeptime + ":0" + timeSecond, (windowWidth/2)-110, (windowHeight/2)+300);
+      if(brewing == true) {
+        print(steeptime + ":" + timeSecond);
+        text('your tea will be ready in ' + timeSecond + ' seconds.', (windowWidth/2), (windowHeight/2)+300);
+      }
     }
-  } else {
-    if(brewing == true) {
-      print(steeptime + ":" + timeSecond);
-      text('your tea will be ready in ' +steeptime + ":" + timeSecond, (windowWidth/2)-110, (windowHeight/2)+300);
-    }
-  }
   
   //draw mug
   noStroke();
   fill(10, 10, 10);
-  rect((windowWidth/2)-90, (windowHeight/2)-100, 250, 200);
+  rect((windowWidth/2)-125, (windowHeight/2)-100, 250, 200);
   fill(10, 10, 10);
-  ellipse((windowWidth/2)+35, (windowHeight/2)+100, 250, 115);
-  ellipse((windowWidth/2)+35, (windowHeight/2)-105, 250, 115);
+  ellipse((windowWidth/2), (windowHeight/2)+100, 250, 115);
+  ellipse((windowWidth/2), (windowHeight/2)-105, 250, 115);
   fill(interA);
-  ellipse((windowWidth/2)+35, (windowHeight/2)-100, 240, 115);
+  ellipse((windowWidth/2), (windowHeight/2)-100, 240, 115);
 }
 
 // change text once steeping begins
@@ -158,7 +153,7 @@ function countDown(){
   
   // second timer
   timeSecond = secondStart - minusSecond ;
-  steeptime = steeptime - minusMinute ;
+  steeptime = steeptime ;
   
   //keep track of last second
   lastSecond = second();
